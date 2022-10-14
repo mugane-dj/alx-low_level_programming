ff6b3186b5472b111a791d708a19653adf70aa8d
mugane <muganes25@gmail.com>
2022-10-14 00:51:41 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints "Programming quote"
+ *
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ * Return: zero
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle")
+	return (0);
+}
