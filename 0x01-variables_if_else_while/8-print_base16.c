f5067e1abea26257c68588cb37afdb8de8e78366
mugane <muganes25@gmail.com>
2022-10-15 03:51:15 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Prints base 16 numbers in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+	char num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+	for (ch = 97; ch <= 102; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
