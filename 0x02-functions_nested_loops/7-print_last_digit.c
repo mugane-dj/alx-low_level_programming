621b87c201e3f832192d9f04360b99808e01c2d1
mugane <muganes25@gmail.com>
2022-10-18 07:26:59 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,8 +10,10 @@
 
 int print_last_digit(int n)
 {
+	/*assign last digit to new int variable digit*/
 	int digit = n % 10;
-
+	
+	/*check if last digit is negative and multiply with -1*/
 	if (digit < 0)
 		digit *= -1;
 
