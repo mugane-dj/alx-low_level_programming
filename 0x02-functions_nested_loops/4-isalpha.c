2ad6086706adca0f4d9d0bc7d79f2599082acd1e
mugane <muganes25@gmail.com>
2022-10-18 06:16:59 -0700

    is alphabet character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - entry point
+ * @c: character to be checked
+ *
+ * Desc: Checks if character is an alphabetic character
+ * Return: 1 if c is a letter,lowercase or uppercase and 0 if otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
