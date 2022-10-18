5d927822f0ccd8a6b006079f1ca6772414244946
mugane <muganes25@gmail.com>
2022-10-18 05:54:53 -0700

    is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - entry point
+ *
+ * Desc: Check if a character is lowercase
+ * Return: 1 if c is lowercase or 0 is otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+	
+	_putchar('\n');
+}
