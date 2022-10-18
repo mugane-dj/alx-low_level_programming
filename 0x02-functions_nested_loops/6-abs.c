d32788fcbda9a3a4ed9ccf4a3265d88fedab68f1
mugane <muganes25@gmail.com>
2022-10-18 06:47:08 -0700

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,19 +3,22 @@
 
 /**
  * _abs - entry point
- * @int: integer to be computed
+ * @n: integer to be computed
+ *
  * Desc: Ouput's the absolute value of an integer
  * Return: Always 0
  */
 
 int _abs(int)
 {
-	if (int >= 0)
+	int n;
+
+	if (n >= 0)
 	{
-		return (int);
+		return (n);
 	}
-	else if (int < 0)
+	else if (n < 0)
 	{
-		return (-int);
+		return (-n);
 	}
 }
