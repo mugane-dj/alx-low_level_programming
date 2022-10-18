47f930ae9185ea5ce6bc726bef4748b5aa441fdc
mugane <muganes25@gmail.com>
2022-10-18 08:45:12 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,6 +20,7 @@ void times_table(void)
 			product = (n * multiple);
 
 			_putchar(product + '0');
+			multiple++;
 
 			if (product <= 9)
 			{
@@ -36,6 +37,7 @@ void times_table(void)
 				_putchar((product % 10) + '0');
 			}
 		}
+		n++;
 		_putchar('\n');
 	}
 
