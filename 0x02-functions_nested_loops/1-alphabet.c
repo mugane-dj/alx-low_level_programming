0213495f9998c48745e3ea878bbbf50c2c19ef5f
mugane <muganes25@gmail.com>
2022-10-18 05:15:45 -0700

    lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main - entry point
+ * print_alphabet - entry point
  *
  * Desc: prints the alphabet in lowercase
  * Return: Always 0
  */
 
-int main(void)
+void print_alphabet(void)
 {
 	char c = 'a';
 
