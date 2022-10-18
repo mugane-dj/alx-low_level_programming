f1ce6ae71e456cbe3f04a5709d54f906411314a0
mugane <muganes25@gmail.com>
2022-10-18 05:44:51 -0700

    multiple alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Desc: Prints the alphabet in lowercase 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 1;
+
+	while (i <= 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		i++;
+	}
+
+	_putchar('\n');
+}
