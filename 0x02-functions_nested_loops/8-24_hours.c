54f57b50785bd5a081c148eed6d706cffb66b4a6
mugane <muganes25@gmail.com>
2022-10-18 07:38:36 -0700

    Jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - entry point
+ *
+ * Desc: prints every minute of the day of Jack Bauer
+ */
+
+void jack_bauer(void)
+{
+	int minutes = 0;
+	int hours = 0;
+
+	while (hours < 24)
+	{
+		while (minutes < 60)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
