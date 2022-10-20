1e041507fe2d0132d6f4c969a7562754f185e876
mugane <muganes25@gmail.com>
2022-10-20 03:37:48 -0700

    0>972

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,4 +6,14 @@
   * @i: integer variable to be checked
   */
 
-void positive_or_negative(int i)
+void positive_or_negative(int i);
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c);
