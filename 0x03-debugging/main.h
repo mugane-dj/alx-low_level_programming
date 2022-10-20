f8ffe3863bdfbfac9f7ae3b352f23a91e21093e9
mugane <muganes25@gmail.com>
2022-10-20 04:40:46 -0700

    header file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -29,4 +29,14 @@ int largest_number(int a, int b, int c);
 
 void print_remaining_days(int month, int day, int year);
 
+/**
+* convert_day - converts day of month to day of year, without accounting
+* for leap year
+* @month: month in number format
+* @day: day of month
+* Return: day of year
+*/
+
+int convert_day(int month, int day);
+
 #endif
