fded4ac45648748b84eef7c2ee4976549644e71a
mugane <muganes25@gmail.com>
2022-10-20 02:54:19 -0700

    mutliple mains

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+  * positive_or_negative - checks if i is psootive, negative or zero
+  * @i: integer variable to be checked
+  */
+
+void positive_or_negative(int i)
