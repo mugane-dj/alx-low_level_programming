c9ee8f802efc55992682d8bb564120625a57ce16
mugane <muganes25@gmail.com>
2022-10-21 03:10:38 -0700

    most

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,38 +1,10 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
-/**
-*_isupper - checks if character is uppercase
-*@c: character to be checked
-*
-*Return: 1 if c is uppercase, 0 if otherwise
-*/
-
 int _isupper(int c);
-
-/**
-*_isdigit - checks if integer is a digit between 0 and 9
-*@c: integer to be checked
-*
-*Return: 1 if c is a digit between 0 and 9, 0 if otherwise
-*/ 
 int _isdigit(int c);
-
-/**
-*mul - multiplies two integers
-*@a - first integer
-*@b - second integer
-*
-*Return: value after multiplication
-*/
-
 int mul(int a, int b);
-
-/**
-*print_numbers - prints all numbers between 0 and 9
-*/
-
 void print_numbers(void);
-
 int _putchar(char c);
+void print_most_numbers(void);
 #endif
