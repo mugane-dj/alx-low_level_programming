c64a9f2143ed7eac854a809a5a1012eb8d6b575a
mugane <muganes25@gmail.com>
2022-10-21 00:12:26 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+*_isupper - checks if character is uppercase
+*@c: character to be checked
+*
+*Return: 1 if c is uppercase, 0 if otherwise
+*/
+
+int _isupper(int c);
+
+#endif
