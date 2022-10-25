8a85a350abcd972f2c4846e3b353e07006011a0a
mugane <muganes25@gmail.com>
2022-10-25 04:26:07 -0700

    putchar

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+* _strlen - prints the length of a string
+*
+* @s: pointer to the string
+* Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+
+}
+
+/**
 * _puts - prints a string, followed by a new line to stdout
 *
 * @str: string to be printed
