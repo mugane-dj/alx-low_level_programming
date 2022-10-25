20a83abd00e14d4b77c00f91c5322b059a5db9b5
mugane <muganes25@gmail.com>
2022-10-25 07:35:02 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+* @l: The character to write
+*
+* Desc: writes a character l to stdout
+* Return: 1 on success and -1 on error
+*/
+
+int _putchar(char c)
+{
+	 return (write(1, &c, 1));
+}
+/**
+* _strlen - prints the length of a string
+*
+* @s: pointer to the string
+* Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+
+}
+
+/**
+* print_rev - prints a string in reverse followed by a new line.
+*
+* @s: string to be printed
+* Return: void
+*/
+
+void print_rev(char *s)
+{
+	int len;
+	int i;
+
+	len = _strlen(s);
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
+
+/**
+* rev_string - reverses a string
+* @s: string to be reversed
+* Return: void
+*/
+
+void rev_string(char *s)
+{
+	print_rev(s);
+}
+
