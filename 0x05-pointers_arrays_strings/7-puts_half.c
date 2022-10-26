33919ab5b603f853d2b22f5b6f8e11b0210ce7d0
mugane <muganes25@gmail.com>
2022-10-26 03:30:04 -0700

    half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+* _strlen - prints the length of a string
+*
+* @s: pointer to the string
+* Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+
+}
+
+/**
+* puts_half - prints the second half of a string followed by a new line
+*
+* @str: string to be printed
+* Return: void
+*/
+
+void puts_half(char *str)
+{
+	int len;
+	int i;
+
+	len = _strlen(str);
+
+	if (len % 2 != 0)
+		i = (len / 2) + 1;
+	else
+		i = len / 2;
+
+	for (; i < len ; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
