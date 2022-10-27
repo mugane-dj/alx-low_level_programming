c975f0e2ace9c7a8fde1d3bec8414fc0208e34a2
mugane <muganes25@gmail.com>
2022-10-27 06:23:25 -0700

    string comparison

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strcmp - compares two strings
+*
+* @s1: first string.
+* @s2: second string.
+* Return: 0 if *s1 == *s2
+*         negative difference if *s1 < *s2
+*         positive difference if *s1 > *s2
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+
+	for (; *s1 == *s2 ;)
+	{
+		s1++;
+		s2++;
+		return (0);
+	}
+	/**
+	* Difference of ASCII values of the 1st character
+	* in both strings
+	*/
+
+	return (*s1 - *s2);
+}
