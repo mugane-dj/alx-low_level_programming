bcf4573276fa023c28c5c3dff825769e7707b111
mugane <muganes25@gmail.com>
2022-11-01 08:58:34 -0700

    substring match

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -19,7 +19,7 @@ char *_strstr(char *haystack, char *needle)
 		if (*needle == 0)
 			return (haystack);
 		for (i = 0; haystack[i] == needle[i]; i++)
-		{	
+		{
 			if (needle[i + 1] == '\0')
 				return (haystack);
 		}
