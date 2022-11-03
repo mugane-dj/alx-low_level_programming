68fa10222a4f2ff0a72036ec9307846277e31a7b
mugane <muganes25@gmail.com>
2022-11-03 06:54:10 -0700

    reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion - prints a string in reverse.
+*
+* @s: pointer to character string.
+* Return: void.
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		/*access memeory address of subsequent character*/
+		_print_rev_recursion(&(s[1]));
+
+		_putchar(*s);
+	}
+	else
+		_putchar('\n');
+}
