772c1f9bbbad05d3b25a34dfc7e8818bb57653c8
mugane <muganes25@gmail.com>
2022-11-07 01:58:55 -0800

    all args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - prints all arguments passed to a program.
+*
+* @argc: argument counter.
+* @argv: pointer to array of strings.
+* Return: 0 on success.
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		int count;
+
+		for (count = 0; count < argc; count++)
+		{
+			printf("%s\n", argv[count]);
+		}
+					
+	}
+	return (0);
+}
