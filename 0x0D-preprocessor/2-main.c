89b3f271f11fba9f5a48166acfa9d2a439f68896
mugane <muganes25@gmail.com>
2022-11-15 01:29:14 -0800

    file macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - prints name of file using file macro.
+*
+*Return: int
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
