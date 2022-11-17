87414a9915d8c11bf803cb9955bc2043bfb39f86
mugane <muganes25@gmail.com>
2022-11-17 04:57:22 -0800

    operations

---
+++ 0x0F-function_pointers/3-op-functions.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+* op_add - returns sum of int.
+*
+* @a: first int member.
+* @b: second int member.
+* Return: sum.
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
