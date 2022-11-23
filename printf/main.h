ebb65b02b1de64c7d545c19906d4a8c6f9c78cd0
mugane <muganes25@gmail.com>
2022-11-23 08:24:55 -0800

    task0

--- printf/main.h
+++ printf/main.h
@@ -1,22 +1,10 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
-#include <stdlib.h>
-#include <stdarg.h>
-
-/**
-* struct converter_struct - A new type defining a converter structure.
-* @specifier: character representing a format specifier i.e %, c.
-* @func: A function pointer pointing to converter function.
-*/
-
-typedef struct converter_struct
-{
-	unsigned char specifier;
-	unsigned int (*func)(va_list);
-} converter;
-
+char *_strrev(char *str);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_itoa(int i, char *output, int base);
 int _printf(const char *format, ...);
-unsigned int (*match_specifier(const char *specifier))(va_list);
 
 #endif
