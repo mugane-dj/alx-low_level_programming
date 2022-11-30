b428e1827ff222f7fb7af708341a0c57d1e0bcf3
mugane <muganes25@gmail.com>
2022-11-30 06:59:06 -0800

    free

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees listint_t list.
+ * @head: pointer to listint_t list.
+ * Return: Void.
+ */
+
+void free_listint(listint_t *head)
+{
+	while (head != NULL)
+	{
+		free(head->n);
+		free(head);
+		head = head->next;
+	}
+}
