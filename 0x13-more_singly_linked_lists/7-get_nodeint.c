2c951c856c3c93d35ce91c92ee99f5b7db6aad5a
Samuel Mugane <muganes25@gmail.com>
2022-12-01 09:37:23 +0300

    nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,15 +11,18 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int node;
+    if (head == NULL)
+        return NULL;
 
-	for (node = 0; node < index; node++)
-	{
-		if (head == NULL)
-			return (NULL);
+    unsigned int node;
 
-		head = head->next;
-	}
+    for (node = 0; node < index; node++)
+    {
+        if (head == NULL)
+            return (NULL);
 
-	return (head);
+        head = head->next;
+    }
+
+    return (head);
 }
