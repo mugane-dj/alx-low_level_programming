eec69fba8a4efd4cbb8b388a809ec29f6d8af148
mugane <muganes25@gmail.com>
2022-12-05 05:26:00 -0800

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,12 +11,13 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (n == 0)
+		return (-1);
+
 	n = n >> index;
 
 	if (n & 1)
 		return (1);
 	else
 		return (0);
-
-	return (-1);
 }
