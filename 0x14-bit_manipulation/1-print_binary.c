ae60d4d2b7ea3aa1ec60810d965d170a5708089a
mugane <muganes25@gmail.com>
2022-12-05 05:13:04 -0800

    binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,9 +10,19 @@
 
 void print_binary(unsigned long int n)
 {
+	if (n & 1)
+		_putchar('1');
+	else
+		_putchar('0');
+
+	n = n >> 1;
+
 	while (n > 0)
 	{
-		_putchar((n & 1) + '0');
+		if (n & 1)
+			_putchar('1');
+		else
+			_putchar('0');
 		n = n >> 1;
 	}
 }
