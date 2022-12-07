0860dcb419a092e3fb4012fbd76699fdffdb65e6
mugane <muganes25@gmail.com>
2022-12-07 02:31:49 -0800

    create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <stddef.h>
+/**
+ * create_file - creates a text file.
+ *
+ * @filename: name of file to do I/O operations on.
+ * @text_content: NULL terminated string to write to file created.
+ * Return: 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t count;
+	int fd, index;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
+
+	if (fd == -1)
+		fd = open(filename, O_TRUNC | O_WRONLY);
+
+	for (index = 0; text_content[index]; index++)
+		;
+
+	count = write(fd, text_content, index);
+
+	if (count == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
