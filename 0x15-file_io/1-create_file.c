6fcaac8228064f91dba5d1c9b4299f7547101fed
mugane <muganes25@gmail.com>
2022-12-07 02:37:45 -0800

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,10 +26,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
 
 	if (fd == -1)
-	{
 		fd = open(filename, O_TRUNC | O_WRONLY);
-		return (-1);
-	}
 
 	if (text_content != NULL)
 	{
