338c53f9357297014728d90cdf084d5ea4e876ac
mugane <muganes25@gmail.com>
2022-12-07 03:39:41 -0800

    initial

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,85 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * terminate_fd - close file descriptor.
+ *
+ * @fd: file descriptor.
+ * Return: void.
+ */
+
+void terminate_fd(int fd)
+{
+	int output;
+
+	output = close(fd);
+
+	if (output == -1)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * allocate_mem - allocates memory for a buffer.
+ *
+ * @file_name: name of file being allocated buffer for.
+ * Return: A point to the buffer allocated memory.
+ */
+
+char *allocate_mem(char *file_name)
+{
+	char *buff;
+
+	buff = malloc(sizeof(*buff) * 1024);
+
+	if (buff == NULL)
+	{
+		dprintf(STDERR_FILENO, 
+				"Error: Can't write to %s\n", 
+				file_name);
+		exit(99);
+	}
+
+	return (buff);
+}
+
+/**
+ * main - copies contents in one file to another file.
+ *
+ * @argc: number of arguments passed to the program.
+ * argv: Array pointer to file names passed to the program.
+ * Return:
+ *	97 if the number of arguments is not 3.
+ *	98 if the file with text content doesn't exist.
+ *	99 if the program can't create or write to second file.
+ *	100 if the program can't close the file descriptor. 
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, fd, count;
+	char *buff;
+
+	buff = allocate_memory(argv[2]);
+	file_from = open(argv[1], O_RDONLY);
+	fd = read(file_from, buff, 1024);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO,
+				"Usage: cp file_from file_to");
+		exit(97);
+	}
+
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,5 +9,8 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+char *allocate_mem(char *file_name);
+void terminate_fd(int fd);
+
 
 #endif
