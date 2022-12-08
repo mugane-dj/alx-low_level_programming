fb2fedea39f99831afdf7463adc68f6bb6f059d7
Samuel Mugane <muganes25@gmail.com>
2022-12-08 09:40:51 +0300

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -81,6 +81,12 @@ int main(int argc, char *argv[])
 	buff = allocate_mem(argv[2]);
 	file_from = open(argv[1], O_RDONLY);
 	fd = read(file_from, buff, BUFFER_SIZE);
+	
+	if (fd > BUFFER_SIZE)
+	{
+		_putchar("...[Diff had to be removed because it was too long]\n");
+	}
+	
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	if (file_from == -1 || fd == -1)
