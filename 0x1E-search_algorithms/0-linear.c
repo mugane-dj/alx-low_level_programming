bda1ca00df501a9e0d722e4727c56cbfa31e9564
mugane <muganes25@gmail.com>
2023-05-09 00:19:43 -0700

    0x0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,10 +18,10 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t index;
 
-	if (array == NULL)
+	if (array == NULL || size < 1)
 		return (-1);
 
-	for (index = 0; index < size - 1; index++)
+	for (index = 0; index < size; index++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
 
