83e145f80e788eb332805fb8a8a4b0a05b11f18f
mugane <muganes25@gmail.com>
2023-05-09 04:06:48 -0700

    header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,6 +4,7 @@
 
 /*Function Prototypes*/
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t size);
 
 #endif /*SEARCH_ALGOS_H*/
