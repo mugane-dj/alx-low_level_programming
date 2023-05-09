d1e1ad0a4fc2d072fea821ca5fcfa7bd48cd10a6
mugane <muganes25@gmail.com>
2023-05-09 00:20:40 -0700

    0x0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,7 +18,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t index;
 
-	if (array == NULL || size < 1)
+	if (array == NULL)
 		return (-1);
 
 	for (index = 0; index < size; index++)
